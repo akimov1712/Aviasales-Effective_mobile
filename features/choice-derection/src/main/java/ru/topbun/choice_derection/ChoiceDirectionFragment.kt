c310f21922c4428e2cb99105f571d05aff1e3642
topbun <akimov1712@mail.ru>
2024-04-18 16:49:47 +0300

    A screen ChoiceDirection has been typeset

---
+++ features/choice-derection/src/main/java/ru/topbun/choice_derection/ChoiceDirectionFragment.kt
@@ -0,0 +1,16 @@
+package ru.topbun.choice_derection
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ru.topbun.android.BaseFragment
+import ru.topbun.choice_derection.databinding.FragmentChoiceDirectionBinding
+
+
+class ChoiceDirectionFragment : BaseFragment<FragmentChoiceDirectionBinding>(FragmentChoiceDirectionBinding::inflate) {
+
+
+
+}
