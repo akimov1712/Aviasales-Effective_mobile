fb1104d06b857b9aac95a17956f8865c3b61b57b
topbun <akimov1712@mail.ru>
2024-04-19 14:31:21 +0300

    A screen ShowTickets has been typeset

---
+++ features/show-tickets/src/main/java/ru/topbun/show_tickets/ShowTicketsFragment.kt
@@ -0,0 +1,14 @@
+package ru.topbun.show_tickets
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ru.topbun.android.BaseFragment
+import ru.topbun.show_tickets.databinding.FragmentShowTicketsBinding
+
+
+class ShowTicketsFragment : BaseFragment<FragmentShowTicketsBinding>(FragmentShowTicketsBinding::inflate) {
+
+}
